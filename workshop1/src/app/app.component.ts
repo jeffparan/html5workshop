9e8cfbd0cf23150f8efda36e34c20cbb3251910d
issuser <jeff.paran@gmail.com>
2018-09-04 17:25:15 +0800

    first vers

---
+++ workshop1/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { LineItem } from './model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  cart: LineItem[] = [];
+
+  processLineItem(LineItem: LineItem){
+    this.cart.push(LineItem);
+    console.log("cart = ", this.cart);
+  }
+}
